7d80440691102e6327f4316461f92d7c62d992a9
LONS <nn2577958@gmail.com>
2026-02-15 07:17:26 +0900

    Update invite button click handler
    Change alert message and redirect after button click.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const inviteButton = document.getElementById("invite-btn");
+
 if (inviteButton) {
   inviteButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    alert("디스코드에서 LONSBOT을 초대합니다!");
+    e.preventDefault(); // 링크 기본 이동 잠시 막기
+    alert("오늘도 찾아 와주셔서 감사합니다"); // 안내 메시지
+    window.location.href = "https://discord.gg/tnMXbQ65Hk"; // 메시지 확인 후 이동
   });
 }
