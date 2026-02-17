cce333b206e9168f1c7ed2d349a99601c94a7e75
LONS <nn2577958@gmail.com>
2026-02-17 22:43:25 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyiAepd539cBTPwtcVnAR-HJbb8roLJmE",
+  authDomain: "lons-dc24d.firebaseapp.com",
+  projectId: "lons-dc24d",
+  storageBucket: "lons-dc24d.appspot.com",
+  messagingSenderId: "755692328918",
+  appId: "1:755692328918:web:a4eb4563cb862d3eb5b677"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
